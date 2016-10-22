3e7d8454c1c35941d30ad3e5093aa478e961d072
Steve Hair <me@stevenhair.com>
2016-10-22 16:49:41 -0400

    unregularized cost function

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,13 +62,13 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-
-
-
-
-
-
-
+y_matrix = eye(num_labels)(y,:);
+a1 = [ones(m, 1) X];
+z2 = a1*Theta1';
+a2 = [ones(m, 1) sigmoid(z2)];
+z3 = a2*Theta2';
+a3 = sigmoid(z3);
+J = 1/m*sum(sum(-y_matrix.*log(a3) - (1-y_matrix).*log(1-a3)));
 
 
 
