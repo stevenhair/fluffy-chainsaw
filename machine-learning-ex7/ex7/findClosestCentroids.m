11e0183196c09bb9e2f6f774042bd5c06d2d832c
Steve Hair <me@stevenhair.com>
2016-11-13 21:08:21 -0500

    findClosestCentroids

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,11 +21,12 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
+pkg load statistics
 
-
-
-
-
+distances = pdist2(X, centroids, 'sqeuclidean');
+for i = 1:length(distances)
+    idx(i) = find(distances(i,:) == min(distances(i,:)));
+end
 
 % =============================================================
 
