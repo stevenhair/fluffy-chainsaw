01ccbaaa4b72052f71a23cf231e692dc418a8ca3
Steve Hair <me@stevenhair.com>
2016-11-19 18:57:20 -0500

    added regularization to cost function

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,7 +41,7 @@ Theta_grad = zeros(size(Theta));
 %
 
 inner = (X*Theta' - Y);
-J = 1/2*sum((inner.^2)(find(R)));
+J = 1/2*sum((inner.^2)(find(R))) + (lambda/2*sum(sum(Theta.^2))) + lambda/2*sum(sum(X.^2));
 
 X_grad = (inner.*R)*Theta;
 Theta_grad = (inner.*R)'*X;
